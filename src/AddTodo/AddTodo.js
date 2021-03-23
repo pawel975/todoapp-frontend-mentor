a44fd41cba60c37d29bef1976484e86c061797d9
Pawel <pawel.kurek975@gmail.com>
2021-03-24 00:06:40 +0100

    initial commit

---
+++ src/AddTodo/AddTodo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './AddTodo';
+
+function AddTodo() {
+    return (
+        <div className="add-todo">
+            <label>
+                <button>XD</button>
+                <input type="text" />
+            </label>
+        </div>
+    )
+}
+
+export default AddTodo;
---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import AddTodo from '../AddTodo/AddTodo';
+
+function App() {
+  return (
+    <div className="app">
+      <header></header>
+      <AddTodo />
+    </div>
+  );
+}
+
+export default App;
